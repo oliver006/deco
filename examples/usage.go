54685e6530534c5ced4393c35fd153ee15e722c6
Oliver <oliver@21zoo.com>
2026-06-24 22:06:56 -0700

    Add MAC-specific usage example
    Backport of mathiastornblom/go-deco@b0e70cbb8cf05a8bb9341b84a22dcaf33f7bc3bd, adapted for this repo's public API. Omit upstream bin/ artifacts.

--- examples/usage.go
+++ examples/usage.go
@@ -2,15 +2,34 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/mrmarble/deco"
 )
 
+const applicationVersion = "1.0.4"
+
 func main() {
-	c := deco.New("192.168.1.1")
-	err := c.Authenticate("router_password")
+	host := flag.String("host", "192.168.1.1", "The host address of the Deco API")
+	password := flag.String("password", "", "The password for authentication")
+	version := flag.Bool("version", false, "Print the application version")
+	flag.Parse()
+
+	if *version {
+		fmt.Printf("Application version: %s\n", applicationVersion)
+		return
+	}
+	if *password == "" {
+		fmt.Println("Usage: usage --host 192.168.1.1 --password router_password")
+		fmt.Println("Usage: usage --version")
+		os.Exit(1)
+	}
+
+	c := deco.New(*host)
+	err := c.Authenticate(*password)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
@@ -18,10 +37,11 @@ func main() {
 	printPerformance(c)
 	printDevices(c)
 	printDecos(c)
+	printDevicesByDeco(c)
 }
 
 func printPerformance(c *deco.Client) {
-	fmt.Println("[+] Permormance")
+	fmt.Println("[+] Performance")
 	result, err := c.Performance()
 	if err != nil {
 		log.Fatal(err.Error())
@@ -40,6 +60,30 @@ func printDevices(c *deco.Client) {
 	for _, device := range result.Result.ClientList {
 
 		fmt.Printf("%s\tOnline: %t\n", device.Name, device.Online)
+	}
+}
+
+func printDevicesByDeco(c *deco.Client) {
+	fmt.Println("[+] Clients by Deco")
+	result, err := c.DeviceList()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	for _, device := range result.Result.DeviceList {
+		deviceName := device.Nickname
+		if deviceName == "" {
+			deviceName = device.DeviceIP
+		}
+		fmt.Printf("%s (%s)\n", deviceName, device.MAC)
+
+		clients, err := c.ClientListForDevice(device.MAC)
+		if err != nil {
+			log.Printf("failed to fetch clients for %s: %s", device.MAC, err.Error())
+			continue
+		}
+		for _, client := range clients.Result.ClientList {
+			fmt.Printf("  %s\tOnline: %t\n", client.Name, client.Online)
+		}
 	}
 }
 
